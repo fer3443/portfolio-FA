4126d03294e358f2c61aa3a47e90bf5030e1a0f4
Fernando <fer3443@gmail.com>
2023-10-26 02:25:15 -0300

    [desing] secciones varias creadas, emailjs agregado!

--- index.js
+++ index.js
@@ -22,3 +22,32 @@ function linkAction() {
     navMenu.classList.remove('showMenu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))//recorro la nodeList creada por querySelectorAll
+//efecto blur al header
+function blurHeader() {
+    const header = document.querySelector('#header')
+    this.scrollY >= 50 ? header.classList.add('blurHeader') : header.classList.remove('blurHeader')
+}
+window.addEventListener('scroll', blurHeader);
+
+//para manejar EmailJs
+const contactForm = document.querySelector('#contactForm')
+const contactMessage = document.querySelector('#contactMessage')
+
+const enviarEmail = (e) => {
+    e.preventDefault()
+
+    //serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_z0jsv7t', 'template_y4ml7s4','#contactForm', 'g-kIBePYIAvhw72xf')
+    .then(() => {
+        contactMessage.textContent = 'Mensaje enviado con exito ✅'
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000)
+        contactForm.reset()
+    }, () => {
+        contactMessage.textContent = 'Mensaje no enviado (error en el servidor)❌',
+        console.log(error)
+    })
+}
+
+contactForm.addEventListener('submit', enviarEmail)
